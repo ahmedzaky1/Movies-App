f6edbeb1a6b1fb610bfbff76cd35a09a3fae2bd8
‪Ahmed Zaky‬‏ <103897664+ahmedzaky1@users.noreply.github.com>
2023-11-15 15:45:54 +0200

    Add files via upload

---
+++ src/views/HomeView.vue
@@ -0,0 +1,208 @@
+<template>
+  <div class="home">
+    <div class="feature-card">
+      <router-link to="/movie/9353b834">
+        <img src="https://i.pinimg.com/564x/46/fe/8d/46fe8d8009be97733d304fc4f7eb376d.jpg" alt="Anime"
+          class="featured-img" />
+        <div class="detial">
+          <h3>One Piece</h3>
+          <p>
+            One Piece (stylized in all caps) is a Japanese manga series written
+            and illustrated by Eiichiro Oda
+          </p>
+        </div>
+      </router-link>
+    </div>
+    <form @submit.prevent="SearchMovies()" class="search-box">
+      <input type="text" v-model="search" placeholder="What are you looking for?" />
+      <input type="submit" value="Search" />
+    </form>
+    <div class="movies-list">
+      <div class="movie" v-for="movie in movies" :key="movie.imdbID">
+        <router-link :to="'/movie/' + movie.imdbID" class="movie-link">
+          <div class="product-img">
+            <img :src="movie.Poster" alt="Movie Poster">
+            <div class="type">{{ movie.Type }}</div>
+          </div>
+          <div class="detail">
+            <p class="year">{{ movie.Year }}</p>
+            <h3>{{ movie.Title }}</h3>
+          </div>
+        </router-link>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+import { ref } from "vue";
+import env from "@/env.js";
+export default {
+  setup() {
+    const search = ref("");
+    const movies = ref([]);
+    // const apikey = "9353b834";
+
+    const SearchMovies = () => {
+      if (search.value != "") {
+        fetch(`http://www.omdbapi.com/?apikey=${env.apikey}&s=${search.value}`)
+          .then((response) => response.json())
+          .then((data) => {
+            movies.value = data.Search;
+            search.value = "";
+            console.log(movies.value);
+          });
+      }
+    };
+
+    return {
+      search,
+      movies,
+      SearchMovies,
+    };
+  },
+};
+</script>
+
+<style lang="scss">
+.home {
+  .feature-card {
+    position: relative;
+
+    .featured-img {
+      display: block;
+      width: 100%;
+      height: 300px;
+      object-fit: cover;
+      position: relative;
+      z-index: 0;
+    }
+
+    .detial {
+      position: absolute;
+      left: 0;
+      right: 0;
+      bottom: 0;
+      background-color: rgba(0, 0, 0, 0.6);
+      padding: 16px;
+      z-index: 1;
+
+      h3 {
+        color: #fff;
+        margin-bottom: 16px;
+      }
+
+      p {
+        color: #fff;
+        font-size: 15px;
+      }
+    }
+  }
+
+  .search-box {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    padding: 16px;
+
+    input {
+      display: block;
+      appearance: none;
+      border: none;
+      outline: none;
+      background: none;
+
+      &[type="text"] {
+        width: 100%;
+        color: #fff;
+        background-color: #496583;
+        font-size: 17px;
+        padding: 10px 16px;
+        border-radius: 8px;
+        margin-bottom: 15px;
+        transition: 0.4s;
+
+        &::placeholder {
+          color: #f3f3f3;
+        }
+
+        &:focus {
+          box-shadow: 0px 3px 6px rgba(0, 0, 0, 0.2);
+        }
+      }
+
+      &[type="submit"] {
+        width: 100%;
+        max-width: 300px;
+        background-color: #42b883;
+        padding: 16px;
+        border-radius: 8px;
+        color: #fff;
+        font-size: 17px;
+        text-transform: uppercase;
+        transition: 0.4s;
+
+        &:active {
+          background-color: #3b8070;
+        }
+      }
+    }
+  }
+  .movies-list {
+    display: flex;
+    flex-wrap: wrap;
+    margin: 0px 8px;
+
+    .movie {
+      max-width: 50%;
+      flex: 1 1 50%;
+      padding: 16px 8px;
+
+      .movie-link {
+        display: flex;
+        flex-direction: column;
+        height: 100%;
+
+        .product-img {
+          position: relative;
+          display: block;
+
+          img {
+            display: block;
+            width: 100%;
+            height: 275px;
+            object-fit: cover;
+          }
+          .type {
+            position: absolute;
+            padding: 8px 16px;
+            background-color: #42b883;
+            color: #fff;
+            bottom: 16px;
+            left: 0px;
+            text-transform: capitalize;
+          }
+        }
+        .detail {
+          background-color: #496583;
+          padding: 16px 8px;
+          flex: 1 1 100%;
+          border-radius: 0px 0px 8px 8px;
+
+          .year {
+            color: #AAA;
+            font-size: 14px;
+          }
+
+          h3 {
+            color: #fff;
+            font-weight: 600px;
+            font-size: 18px;
+          }
+        }
+      }
+    }
+  }
+}
+</style>
